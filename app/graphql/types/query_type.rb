9cd39ed083af93696f933f625567cef550a6f0bb
Moiz Ali <amoiz4142@gmail.com>
2021-07-29 01:43:37 +0500

    Init commit with RSpec magic.

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,27 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    # Add root-level fields here.
+    # They will be entry points for queries on your schema.
+
+    # TODO: remove me
+    # field :test_field, String, null: false,
+    #   description: "An example field added by the generator"
+    # def test_field
+    #   "Hello World!"
+    # end
+
+    field :items, [Types::ItemType], 
+      null: false, 
+      description: "Returns a list of items in martian library"
+
+    def items
+      # Item.all
+      # To avoid N+1 query problems, eager load the user
+      Item.preload(:user)
+    end
+  end
+end
